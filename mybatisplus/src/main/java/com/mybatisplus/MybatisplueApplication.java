29ebbbc24909b55d34eda1a476d937e2a2cc2fcf
liu-jia-jun <3142467441@qq.com>
2021-12-15 15:49:07 +0800

    事务和乐观锁，悲观锁

--- mybatisplus/src/main/java/com/mybatisplus/MybatisplueApplication.java
+++ mybatisplus/src/main/java/com/mybatisplus/MybatisplueApplication.java
@@ -4,6 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author 刘佳俊
+ */
 @SpringBootApplication
 @MapperScan(value = "com.mybatisplus.dao.mapper")
 public class MybatisplueApplication {
