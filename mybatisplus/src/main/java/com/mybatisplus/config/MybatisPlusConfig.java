b313916f8265ec6a5c1163811f38a08c35c26f65
liu-jia-jun <3142467441@qq.com>
2021-12-15 19:41:30 +0800

    update

--- mybatisplus/src/main/java/com/mybatisplus/config/MybatisPlusConfig.java
+++ mybatisplus/src/main/java/com/mybatisplus/config/MybatisPlusConfig.java
@@ -2,8 +2,10 @@ package com.mybatisplus.config;
 
 import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.aop.interceptor.PerformanceMonitorInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 /**
  * @author 刘佳俊
@@ -31,5 +33,22 @@ public class MybatisPlusConfig {
         return new PaginationInnerInterceptor();
     }
 
+    /**
+     * SQL 执行性能分析插件
+     * 开发环境使用，项目上线之后无需开启
+     * maxTime 知道是，sql最大的执行时间
+     * 如果sql执行的时间超过maxIime就会抛出异常，但是sql语句可以执行成功
+     *
+     * 该功能需要引入依赖
+     * p6spy
+     */
 
+//    @Bean
+//    @Profile({"dev","test"})// 设置 dev test 环境开启
+//    public PerformanceInterceptor performanceInterceptor() {
+//        PerformanceInterceptor performanceInterceptor =  new PerformanceInterceptor();
+//        performanceInterceptor.setFormat(true);//格式化语句
+//        //performanceInterceptor.setMaxTime(5);//执行时间超过多少秒会抛出异常
+//        return  performanceInterceptor;
+//    }
 }
